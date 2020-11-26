c09d95b9bce1630a99913ef7399a31a8ea412df3
NimbleWing <1428116329@qq.com>
2020-11-26 15:11:52 +0800

    fix: update field to package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Figlet from 'figlet';
 import Chalk from 'chalk';
+import Inquirer from 'inquirer';
+import Fs from 'fs';
 import Pkg from '../package.json';
 import loadTemplate from './util/loadTemplate';
 import {
@@ -10,12 +12,40 @@ import {
 
 console.log(Chalk.yellow.bold(Figlet.textSync(Pkg.name.split('/')[1])));
 console.log(Chalk.yellow.bold(`version: ${Pkg.version}`));
-
+async function rewritePackageFile($githubName, $moduleName) {
+  const pkgData = JSON.parse(Fs.readFileSync('./package.json'));
+  pkgData.name = $moduleName;
+  delete pkgData.bin;
+  pkgData.repository = {
+    type: 'git',
+    url: `git+https://github.com/${$githubName}/${$moduleName}.git`,
+  };
+  pkgData.author = `${$githubName} (http://github.com/${$githubName})`;
+  pkgData.bugs = {
+    url: `https://github.com/${$githubName}/${$moduleName}/issues`,
+  };
+  pkgData.homepage = `https://github.com/${$githubName}/${$moduleName}#readme`;
+  Fs.writeFileSync('./package.json', JSON.stringify(pkgData, null, 2));
+}
 (async () => {
   try {
+    const questions = [
+      {
+        type: 'input',
+        name: 'githubName',
+        message: 'What\'s your github name',
+      },
+      {
+        type: 'input',
+        name: 'nodeModuleName',
+        message: 'What\'s your node module name',
+      },
+    ];
+    const answers = await Inquirer.prompt(questions);
     logWithSpinner('Loading template');
     await loadTemplate('./');
     stopSpinner();
+    await rewritePackageFile(answers.githubName, answers.nodeModuleName);
   } catch (error) {
     failSpinner('Load template failed');
     console.log(error);
